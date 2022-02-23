461fd71ae603fc44bc1e090affc5cf53079cb791
PARKGICHEOL <99226744+PARKGICHEOL@users.noreply.github.com>
2022-02-23 11:32:47 +0900

    Delete script directory

--- script/script.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $(".header_left> li").mouseover(function(){
-        $(this).children(".sub").stop().slideDown();
-    });
-    $(".header_left> li").mouseout(function(){
-        $(this).children(".sub").stop().slideUp();
-    });
-});
